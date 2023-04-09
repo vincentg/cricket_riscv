de1881a39fcd17aa507f16853948d95996af9741
Vincent Gerard <vincentpgerard@gmail.com>
2023-04-09 16:55:42 +0000

    Implemented input parsing

--- main.s
+++ main.s
@@ -31,11 +31,13 @@ displaychar: .ascii "OX/ "
 invalidinput: .ascii "Error: Invalid input, please retry (ENTER for out, <num> for number between 15 and 20\n d<num> for double\n t<num> for triples\n\n"
 .equ invalidinput_len, 126
 
+# Start of constants
 .equ stdin_fd, 0
 .equ stdout_fd, 1
 .equ read_syscall, 63
 .equ write_syscall, 64
 
+#Start of Macros
 .macro print addr len
 li a0, stdout_fd
 la a1, \addr
@@ -56,15 +58,15 @@ _start:
   
     turn_start:
         li t0, '1' 
-        add a3, t0, a4 # a3 <- '1' + playerid
-        la a1, playerturn #offset of player number
-        sb a3, playerid_pos(a1)
+        add a3, t0, a4 # a3 <- '1' + playerid (a4)
+        la a1, playerturn 
+        sb a3, playerid_pos(a1) #Override playerId
         print playerturn, playerturn_len
 
         dart_start:    
-            add a3, t0, a5
+            add a3, t0, a5 # a3 <- '1' + dartId (a5)
             la a1, dartprompt
-            sb a3, dartnum_pos(a1)
+            sb a3, dartnum_pos(a1) #Override dartId in msg
             print dartprompt dartprompt_len
 
             li a0, stdin_fd
@@ -74,15 +76,55 @@ _start:
             ecall
             # a0 = num read
             # a1 contain read bytes
+
+            # case 1, 1 byte read (ENTER)
+            li a2,1
+            beq a0,a2,next_dart
+
+            # case 2, 2 byte read (invalid)
+            addi a2,a2,1
+            beq a0,a2,invalid_entry
             
-            # invalid entry, print error and redo imput
+            # Start of valid input, init mult ratio to 1
+            li t1, 1
+            # case 3, 3 byte read (accept number 15-20)
+            addi a2,a2,1
+            beq a0,a2,read_number
+            
+            # case 4, 4 byte read (d<num> , t<num>)
+            lb a0,(a1)
+            # Move a1 1 byte forward
+            addi a1,a1,1
+            li a2, 't'
+            beq a0,a2,set_triple  # First letter = 't'
+            li a2, 'd'
+            beq a0,a2,set_double  # Fist letter = 'd'
+            j invalid_entry # nor t or d
+
+            set_triple:
+            li t1,3
+            j read_number
+
+            set_double:
+            addi t1,t1,1
+            
+
+            read_number:
+            # t1 contains multiplier (1,2,3), a1 "<dgt><dgt>\n"
+            ebreak
+            
+
+            invalid_entry:
+            # invalid entry, print error and redo input
             print invalidinput, invalidinput_len
             j dart_start
-            
-            
 
-            
+            update_score:
 
+            next_dart:
+                addi a5,a5,1
+                j dart_start
+            
 
     #SHOW GameState at end of turn
     li a0, 1 #stdout
@@ -146,13 +188,6 @@ _start:
                 add x6, x6, x9
                 j input_next
                 
-            set_double:
-                li x12, 2
-                j input_next
-                
-            set_triple:
-                li x12, 3
-            
             input_next:
                 addi x11, x11, 1
                 j read_input
